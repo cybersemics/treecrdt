3fc8805c706c71b5486addfcf434dfe09f7e0ee5
Marcus Pousette <marcus.pousette@live.com>
2026-03-23 10:51:02 +0100

    style(rust): format postgres changes

--- packages/treecrdt-postgres-napi/native-rs/src/lib.rs
+++ packages/treecrdt-postgres-napi/native-rs/src/lib.rs
@@ -316,9 +316,12 @@ impl PgBackend {
         let client = connect(&self.url)?;
         let client = std::rc::Rc::new(std::cell::RefCell::new(client));
         let parent = bytes16_to_node(&parent).map_err(map_core_err)?;
-        let refs =
-            treecrdt_postgres::list_op_refs_children_with_parent_payload(&client, &self.doc_id, parent)
-                .map_err(map_core_err)?;
+        let refs = treecrdt_postgres::list_op_refs_children_with_parent_payload(
+            &client,
+            &self.doc_id,
+            parent,
+        )
+        .map_err(map_core_err)?;
         Ok(refs.into_iter().map(|r| Buffer::from(r.to_vec())).collect())
     }
 
